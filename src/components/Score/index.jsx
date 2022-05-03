3e94f6931ca4144b02acc8356cfcf32ee7f0653e
DesireeBesnard <desireebesnard@gmail.com>
2022-05-03 17:57:44 +0200

    style: score

--- src/components/Score/index.jsx
+++ src/components/Score/index.jsx
@@ -1,8 +1,20 @@
 import { RadialBarChart, RadialBar, Legend, ResponsiveContainer } from 'recharts';
+import '../../style/Score.css'
 
 function Score(props) {
 
     let data
+
+    function CustomLegend(props) {
+        const {payload} = props
+        return(
+          <div className='scoreLegend'>
+            <p>Score</p>
+            <p><span>{payload[0].payload.value*100}%</span><br/>de votre <br/> objectif</p>
+          </div>
+        )
+      }
+    
 
     
     if(props.loading === false) {
@@ -16,16 +28,25 @@ function Score(props) {
 
     return (
         <ResponsiveContainer width="33%" height="100%">
-            <RadialBarChart cx="50%" cy="50%" innerRadius="80%" outerRadius="100%" barSize={10} data={data}>
+            <RadialBarChart 
+                cx="50%" 
+                cy="50%" 
+                innerRadius="80%" 
+                outerRadius="100%" 
+                barSize={10} 
+                data={data}
+                startAngle={90} 
+                endAngle={450}
+            >
             <RadialBar
                 minAngle={15}
                 dataKey="data"
-                denominator = {1}
                 legendType="none"
-                startAngle={90} 
-                endAngle={180}
+                startAngle={0} 
+                endAngle={360}
+                cornerRadius={5}
             />
-            <Legend iconSize={10} layout="vertical" verticalAlign="middle" />
+            <Legend iconSize={10} verticalAlign="middle"  align="right" content={CustomLegend} />
             </RadialBarChart>
         </ResponsiveContainer>
     )
